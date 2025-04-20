a37732176b06802dffaee532f68c9d1a2a778885
Maliuk-Olexandr <maamcd100@gmail.com>
2025-04-20 22:03:02 +0300

    HW4 - Refactor tasks and add console logs for testing functionality

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,23 @@
-function getElementWidth(content, padding, border) {
-  return (
-    Number.parseFloat(content) + (Number.parseFloat(padding) + Number.parseFloat(border)) * 2
-  );
-}
+console.log("----- Task 3: -----");
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
